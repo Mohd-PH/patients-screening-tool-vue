a5b42eea6b06d489b2a2cc93e22a8f3732aaec28
Mohd-PH <9362157+Mohd-PH@users.noreply.github.com>
2021-04-03 01:29:16 +0300

    Add the welcome alert modal

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <WelcomeAlert></WelcomeAlert>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import WelcomeAlert from './components/WelcomeAlert'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    WelcomeAlert
   }
 }
 </script>
