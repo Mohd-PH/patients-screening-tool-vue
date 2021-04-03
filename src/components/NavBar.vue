70ca8f41a11b31809cf2031a0e726a23bda86293
Mohd-PH <9362157+Mohd-PH@users.noreply.github.com>
2021-04-03 10:39:02 +0300

    Target md instead of lg on the navbar collapse

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="WelcomeAlert">
-    <b-navbar toggleable="lg" type="dark" variant="info">
+    <b-navbar toggleable="md" type="dark" variant="info">
       <b-navbar-brand href="#">Patients screening tool</b-navbar-brand>
 
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
