43f9ce0511d4ce9cc22d431eacfaa441427c483c
Mohd-PH <9362157+Mohd-PH@users.noreply.github.com>
2021-04-04 17:43:18 +0300

    Setup screening recommendations section

--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -5,17 +5,22 @@
         <b-col><patients-information v-on:patientInformationSubmitted="patientsInformationData = $event"></patients-information></b-col>
       </b-row>
       <hr>
+      <b-row>
+        <b-col><screeningRecommendations v-bind:patientsInformationData="patientsInformationData"></screeningRecommendations></b-col>
+      </b-row>
     </b-container>
   </div>
 </template>
 
 <script>
 import patientsInformation from './patientsInformation'
+import screeningRecommendations from './screeningRecommendations'
 
 export default {
   name: 'Home',
   components: {
-    patientsInformation
+    patientsInformation,
+    screeningRecommendations
   },
   data () {
     return {
---
+++ src/pages/home/screeningRecommendations.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h3>Screerning recommendations</h3>
+    <b-row class="justify-content-md-center">
+
+    </b-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'screeningRecommendations',
+  props: ['patientsInformationData'],
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
