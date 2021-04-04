12d1dd224ceb93b7c9a878090fa6e65ecaab4062
Mohd-PH <9362157+Mohd-PH@users.noreply.github.com>
2021-04-04 19:23:51 +0300

    Add and test colorectal cancer screening

--- src/pages/home/screeningRecommendations.vue
+++ src/pages/home/screeningRecommendations.vue
@@ -1,24 +1,75 @@
 <template>
   <div>
-    <h3>Screerning recommendations</h3>
-    <b-row class="justify-content-md-center">
-
-    </b-row>
+    <b-container>
+      <h3>Screening recommendations</h3>
+      <b-row class="justify-content-md-center">
+        <b-col lg="6" class="recommendation colorectalCancer">
+          <screeningRecommendationCard
+          v-if="colorectalCancer"
+          :disease="colorectalCancer.disease"
+          :organization="colorectalCancer.organization"
+          :link="colorectalCancer.link"
+          :grade="colorectalCancer.grade"
+          :recommendation="colorectalCancer.recommendation"
+          :date="colorectalCancer.date"
+          ></screeningRecommendationCard>
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
 <script>
+import screeningRecommendationCard from '../../components/screeningRecommendationCard'
+
 export default {
   name: 'screeningRecommendations',
+  components: {
+    screeningRecommendationCard
+  },
   props: ['patientsInformationData'],
   data () {
     return {
 
     }
+  },
+  computed: {
+    colorectalCancer () {
+      if (!this.patientsInformationData) return false
+      if (this.patientsInformationData.age >= 50 && this.patientsInformationData.age <= 75) {
+        return {
+          disease: 'Colorectal cancer screening',
+          organization: 'U.S. Preventive Services Task Force',
+          link: 'https://www.uspreventiveservicestaskforce.org/uspstf/recommendation/colorectal-cancer-screening',
+          grade: 'A',
+          recommendation: `The USPSTF recommends screening for colorectal cancer starting at age 50 years and continuing until age 75 years.
+
+The risks and benefits of different screening methods vary. refer to the link for more information`,
+          date: 'June 15, 2016'
+        }
+      } else if (this.patientsInformationData.age >= 76 && this.patientsInformationData.age <= 85) {
+        return {
+          disease: 'Colorectal cancer screening',
+          organization: 'U.S. Preventive Services Task Force',
+          link: 'https://www.uspreventiveservicestaskforce.org/uspstf/recommendation/colorectal-cancer-screening',
+          grade: 'C',
+          recommendation: `The decision to screen for colorectal cancer in adults aged 76 to 85 years should be an individual one, taking into account the patient's overall health and prior screening history.
+
+Adults in this age group who have never been screened for colorectal cancer are more likely to benefit.
+
+Screening would be most appropriate among adults who 1) are healthy enough to undergo treatment if colorectal cancer is detected and 2) do not have comorbid conditions that would significantly limit their life expectancy.`,
+          date: 'June 15, 2016'
+        }
+      } else {
+        return false
+      }
+    }
   }
 }
 </script>
 
-<style>
-
+<style scoped>
+  .recommendation {
+    padding-bottom: 25px;
+  }
 </style>
---
+++ tests/unit/colorectalCancerScreening.spec.js
@@ -0,0 +1,68 @@
+import {
+  mount,
+  createLocalVue
+} from '@vue/test-utils'
+import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
+import screeningRecommendations from '@/pages/home/screeningRecommendations.vue'
+
+const localVue = createLocalVue()
+localVue.use(BootstrapVue)
+localVue.use(BootstrapVueIcons)
+
+describe('Colorectal cancer screening', () => {
+  // This test follows the colorectal cancer screening recommendations by the U.S. Preventive services Task Force
+  // https://www.uspreventiveservicestaskforce.org/uspstf/recommendation/colorectal-cancer-screening
+  // Last time accessed 04/04/2021
+
+  it('Screens for 80 years old male', async () => {
+    const wrapper = mount(screeningRecommendations, {
+      localVue,
+      propsData: {
+        patientsInformationData: {
+          age: 80,
+          gender: 'Male',
+          smoker: 'No'
+        }
+      }
+    })
+
+    expect(wrapper.find('colorectalCancer').element).toBe()
+    expect(wrapper.text()).toContain('Recommendation Grade: C')
+    expect(wrapper.text()).toContain('Colorectal cancer screening')
+    expect(wrapper.html()).toContain('https://www.uspreventiveservicestaskforce.org/uspstf/recommendation/colorectal-cancer-screening')
+  })
+  it('Screens for 55 years old female', async () => {
+    const wrapper = mount(screeningRecommendations, {
+      localVue,
+      propsData: {
+        patientsInformationData: {
+          age: 55,
+          gender: 'Female',
+          smoker: 'No'
+        }
+      }
+    })
+
+    expect(wrapper.find('colorectalCancer').element).toBe()
+    expect(wrapper.text()).toContain('Recommendation Grade: A')
+    expect(wrapper.text()).toContain('Colorectal cancer screening')
+    expect(wrapper.html()).toContain('https://www.uspreventiveservicestaskforce.org/uspstf/recommendation/colorectal-cancer-screening')
+  })
+  it('Doesn\'t Screen for 49 years old Male', async () => {
+    const wrapper = mount(screeningRecommendations, {
+      localVue,
+      propsData: {
+        patientsInformationData: {
+          age: 49,
+          gender: 'Male',
+          smoker: 'Yes'
+        }
+      }
+    })
+    expect(wrapper.find('colorectalCancer').element).toBeFalsy()
+    expect(wrapper.text()).not.toContain('Recommendation Grade: A')
+    expect(wrapper.text()).not.toContain('Recommendation Grade: C')
+    expect(wrapper.text()).not.toContain('Colorectal cancer screening')
+    expect(wrapper.html()).not.toContain('https://www.uspreventiveservicestaskforce.org/uspstf/recommendation/colorectal-cancer-screening')
+  })
+})
