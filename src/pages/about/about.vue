d8b82993bdca0ad6bcce37dec626de222cacd5f6
Mohd-PH <9362157+Mohd-PH@users.noreply.github.com>
2021-04-12 20:46:34 +0300

    Remove a question from the about page

--- src/pages/about/about.vue
+++ src/pages/about/about.vue
@@ -82,17 +82,6 @@
         </b-card-body>
       </b-collapse>
     </b-card>
-
-    <b-card no-body class="mb-1 mx-3">
-      <b-card-header header-tag="header" class="p-1" role="tab">
-        <b-button block v-b-toggle.accordion-5 variant="info">Does this tool cover pregnant patients?</b-button>
-      </b-card-header>
-      <b-collapse id="accordion-5" accordion="my-accordion" role="tabpanel">
-        <b-card-body>
-          <b-card-text>In the time being, no</b-card-text>
-        </b-card-body>
-      </b-collapse>
-    </b-card>
   </div>
 </template>
 
