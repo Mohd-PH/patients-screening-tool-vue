25b84ff04958777c554e0d7720d32186f6684625
Mohd-PH <9362157+Mohd-PH@users.noreply.github.com>
2021-04-17 04:25:26 +0300

    Add description to recemmondation grades

--- src/components/screeningRecommendationCard.vue
+++ src/components/screeningRecommendationCard.vue
@@ -7,7 +7,7 @@
         <h6 class="mb-0" v-text="organization"></h6>
       </template>
       <b-card-text v-text="recommendation"></b-card-text>
-      <p>Recommendation Grade: <b v-text="grade" class="grade"></b>, dated <em v-text="date"></em></p>
+      <p>Recommendation Grade: <b v-text="grade" class="grade" :title="gradeDescription"></b>, dated <em v-text="date"></em></p>
       <div class="text-right">
         <b-button :href="link" variant="primary" target="_blank">
           Recommendation <b-icon icon="box-arrow-up-right" scale="0.8"></b-icon>
@@ -24,6 +24,22 @@ export default {
   data () {
     return {
 
+    }
+  },
+  computed: {
+    gradeDescription () {
+      if (this.organization === 'U.S. Preventive Services Task Force') {
+        if (this.grade === 'A') {
+          return 'The USPSTF recommends the service. There is high certainty that the net benefit is substantial.'
+        } else if (this.grade === 'B') {
+          return 'The USPSTF recommends the service. There is high certainty that the net benefit is moderate or there is moderate certainty that the net benefit is moderate to substantial.'
+        } else if (this.grade === 'C') {
+          return 'The USPSTF recommends selectively offering or providing this service to individual patients based on professional judgment and patient preferences. There is at least moderate certainty that the net benefit is small.'
+        } else {
+          return null
+        }
+      }
+      return null
     }
   }
 }
