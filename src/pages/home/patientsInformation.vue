ef2064860bafbcc46cecf5005850aa8b0046cc28
Mohd-PH <9362157+Mohd-PH@users.noreply.github.com>
2021-04-17 04:20:37 +0300

    Tidy up the form

--- src/pages/home/patientsInformation.vue
+++ src/pages/home/patientsInformation.vue
@@ -7,43 +7,43 @@
     <b-row class="justify-content-md-center">
       <b-col lg="4">
         <b-form @submit="onSubmit" @reset="onReset">
-          <b-form-group id="input-group-1" label="Age:" label-for="input-age" label-cols-sm="6">
+          <b-form-group id="input-group-1" label="Age:" label-for="input-age" label-cols-sm="5">
             <b-form-input id="input-age" v-model.number="form.age" type="number" placeholder="Enter age" required>
             </b-form-input>
           </b-form-group>
 
-          <b-form-group id="input-group-2" label="Height:" label-for="input-height" label-cols-sm="6">
+          <b-form-group id="input-group-2" label="Height:" label-for="input-height" label-cols-sm="5">
             <b-form-input id="input-height" v-model.number="form.height" type="number" placeholder="Enter height in cm" required>
             </b-form-input>
           </b-form-group>
 
-          <b-form-group id="input-group-3" label="Weight:" label-for="input-weight" label-cols-sm="6">
+          <b-form-group id="input-group-3" label="Weight:" label-for="input-weight" label-cols-sm="5">
             <b-form-input id="input-weight" v-model.number="form.weight" type="number" placeholder="Enter weight in kg" required>
             </b-form-input>
           </b-form-group>
 
-          <b-form-group id="input-group-4" label="Gender:" label-for="input-gender" label-cols-sm="6">
+          <b-form-group id="input-group-4" label="Gender:" label-for="input-gender" label-cols-sm="5">
             <b-button-group id="input-gender">
               <b-button id="gender-male-button" :variant="form.gender === 'Male' ? 'primary' : ''" @click="form.gender = 'Male'">Male</b-button>
               <b-button id="gender-female-button" :variant="form.gender === 'Female' ? 'primary' : ''" @click="form.gender = 'Female'">Female</b-button>
             </b-button-group>
           </b-form-group>
 
-          <b-form-group id="input-group-5" label="Is she postmenopausal:" label-for="input-postmenopausal" label-cols-sm="6" v-if="form.gender === 'Female'">
+          <b-form-group id="input-group-5" label="Is she postmenopausal:" label-for="input-postmenopausal" label-cols-sm="5" v-if="form.gender === 'Female'">
             <b-button-group id="input-postmenopausal">
               <b-button id="postmenopausal-yes-button" :variant="form.postmenopausal === 'Yes' ? 'primary' : ''" @click="form.postmenopausal = 'Yes'">Yes</b-button>
               <b-button id="postmenopausal-no-button" :variant="form.postmenopausal === 'No' ? 'primary' : ''" @click="form.postmenopausal = 'No'">No</b-button>
             </b-button-group>
           </b-form-group>
 
-          <b-form-group id="input-group-5" label="Is she pregnant:" label-for="input-pregnant" label-cols-sm="6" v-if="form.postmenopausal === 'No'">
+          <b-form-group id="input-group-5" label="Is she pregnant:" label-for="input-pregnant" label-cols-sm="5" v-if="form.postmenopausal === 'No'">
             <b-button-group id="input-pregnant">
               <b-button id="pregnant-yes-button" :variant="form.pregnant === 'Yes' ? 'primary' : ''" @click="form.pregnant = 'Yes'">Yes</b-button>
               <b-button id="pregnant-no-button" :variant="form.pregnant === 'No' ? 'primary' : ''" @click="form.pregnant = 'No'">No</b-button>
             </b-button-group>
           </b-form-group>
 
-          <b-form-group id="input-group-6" label="Smoker:" label-for="input-smoker" label-cols-sm="6">
+          <b-form-group id="input-group-6" label="Smoker:" label-for="input-smoker" label-cols-sm="5">
             <b-button-group id="input-smoker">
               <b-button id="smoker-yes-button" :variant="form.smoker === 'Yes' ? 'primary' : ''" @click="form.smoker = 'Yes'">Yes</b-button>
               <b-button id="exsmoker-button" :variant="form.smoker === 'Ex-smoker' ? 'primary' : ''" @click="form.smoker = 'Ex-smoker'">Ex-smoker</b-button>
@@ -51,7 +51,7 @@
             </b-button-group>
           </b-form-group>
 
-          <b-form-group id="input-group-7" label="Pack years:" label-for="input-packYears" label-cols-sm="6" v-if="form.smoker === 'Yes' || form.smoker === 'Ex-smoker'">
+          <b-form-group id="input-group-7" label="Pack years:" label-for="input-packYears" label-cols-sm="5" v-if="form.smoker === 'Yes' || form.smoker === 'Ex-smoker'">
             <b-form-input id="input-packYears" v-model.number="form.packYears" type="number" placeholder="Pack years" required>
             </b-form-input>
           </b-form-group>
